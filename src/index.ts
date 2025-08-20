9e8879546ad2c8fc78fbe611d5a1d35e71d67acb
Xuanho1710 <xuanhodcbas@gmail.com>
2025-08-20 08:45:41 +0700

    17. Finish done lab 1 - task 17

--- src/index.ts
+++ src/index.ts
@@ -357,8 +357,23 @@ class Box<T> {
 }
 
 
+// 17. Write a singleton Logger class that logs messages to console.
+class Logger {
+    private static instance: Logger;
 
+    private constructor() { }
 
+    static getInstance(): Logger {
+        if (!Logger.instance) {
+            Logger.instance = new Logger();
+        }
+        return Logger.instance;
+    }
+
+    log(message: string): void {
+        console.log(message);
+    }
+}
 
 
 
