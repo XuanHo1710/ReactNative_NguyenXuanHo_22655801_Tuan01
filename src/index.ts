ffb83c192630beb268b48e007756f0555815456f
Xuanho1710 <xuanhodcbas@gmail.com>
2025-08-20 07:34:34 +0700

    3. Finish done lab 1 - task 1

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,24 @@ function hello(name: string): string {
 }
 
 console.log(hello("200Lab"));
+
+
+// 1. Create a class Person with attributes name and age. Write a method to display this information.
+
+class Person {
+    name: string;
+    age: number;
+
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+    }
+
+    displayInfo(): string {
+        return `Name: ${this.name}, Age: ${this.age}`;
+    }
+}
+
+console.log(new Person("Nguyen Xuan Ho", 25).displayInfo());
+
+
