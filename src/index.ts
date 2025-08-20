753d33c94ce35ac6bd0f0a8a7e62764d6ca9eae0
Xuanho1710 <xuanhodcbas@gmail.com>
2025-08-20 08:37:01 +0700

    13. Finish done lab 1 - task 13

--- src/index.ts
+++ src/index.ts
@@ -263,6 +263,36 @@ class Fish implements Swimmable {
     }
 }
 
+// 13. Create an abstract class Shape with method area(). Implement Square and Circle.
+abstract class Shape {
+    abstract area(): number;
+}
+class Square extends Shape {
+    side: number;
+
+    constructor(side: number) {
+        super();
+        this.side = side;
+    }
+
+    area(): number {
+        return this.side * this.side;
+    }
+}
+class Circle extends Shape {
+    radius: number;
+
+    constructor(radius: number) {
+        super();
+        this.radius = radius;
+    }
+
+    area(): number {
+        return Math.PI * this.radius * this.radius;
+    }
+}
+
+
 
 
 
