1c67641164c4bff0fa3becfebd1eea2323617819
Xuanho1710 <xuanhodcbas@gmail.com>
2025-08-20 08:42:03 +0700

    16. Finish done lab 1 - task 16

--- src/index.ts
+++ src/index.ts
@@ -339,6 +339,22 @@ class Library {
     }
 }
 
+// 16. Create a generic class Box that can store any type of value.
+class Box<T> {
+    private value: T;
+
+    constructor(value: T) {
+        this.value = value;
+    }
+
+    getValue(): T {
+        return this.value;
+    }
+
+    setValue(value: T): void {
+        this.value = value;
+    }
+}
 
 
 
