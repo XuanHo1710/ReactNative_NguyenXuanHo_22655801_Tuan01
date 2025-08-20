a1d48712b97c128282b0a7994f359cfb307cd76c
Xuanho1710 <xuanhodcbas@gmail.com>
2025-08-20 08:47:32 +0700

    18. Finish done lab 1 - task 18

--- src/index.ts
+++ src/index.ts
@@ -375,6 +375,27 @@ class Logger {
     }
 }
 
+// 18. Create a static class MathUtil with methods add(), subtract(), multiply(), divide().
+class MathUtil {
+    static add(a: number, b: number): number {
+        return a + b;
+    }
+
+    static subtract(a: number, b: number): number {
+        return a - b;
+    }
+
+    static multiply(a: number, b: number): number {
+        return a * b;
+    }
+
+    static divide(a: number, b: number): number {
+        if (b === 0) {
+            throw new Error("Cannot divide by zero.");
+        }
+        return a / b;
+    }
+}
 
 
 
